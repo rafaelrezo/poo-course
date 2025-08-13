ea1eeaf31d0c5ab46d8556584a65837b59399601
Rafael Emerick <rafael.rezo@gmail.com>
2025-08-13 19:58:14 -0300

    chore: adicinando comentarios

--- exercises/solution1.cpp
+++ exercises/solution1.cpp
@@ -4,24 +4,28 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    //tratando input do programa
     if (argc < 3) {
         cerr << "Uso: " << argv[0] << " valor1 valor2 ... valorN valor_referencia" << endl;
         return 1;
     }
-
+    //carregando vetor de entrada
     vector<double> valores;
     for (int i = 1; i < argc - 1; i++) {
         valores.push_back(atof(argv[i]));
     }
 
+    //lendo valor de referencia (ultimo valor da lista de input )
     double referencia = atof(argv[argc - 1]);
 
+    //calculando a media
     double soma = 0;
     for (double v : valores) {
         soma += v;
     }
     double media = soma / valores.size();
 
+    //gerando saida do programa
     cout << "Média: " << media << endl;
     cout << "Valor de referência: " << referencia << endl;
     cout << (media > referencia ? "A média é maior que o valor de referência." 
--- exercises/solution1.py
+++ exercises/solution1.py
@@ -1,14 +1,20 @@
 import sys
 
+#tratando input do programa
 if len(sys.argv) < 3:
     print(f"Uso: {sys.argv[0]} valor1 valor2 ... valorN valor_referencia")
     sys.exit(1)
 
+#carregando os vetores
 valores = list(map(float, sys.argv[1:-1]))
+
+#lendo o valor de referencia
 referencia = float(sys.argv[-1])
 
+#calculando a media
 media = sum(valores) / len(valores)
 
+# gerando saida do programa
 print(f"Média: {media}")
 print(f"Valor de referência: {referencia}")
 if media > referencia:
